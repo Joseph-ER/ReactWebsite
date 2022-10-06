a41be5592249ad6fc7d0f20d0d89af4a441d3e11
Joseph Edwards <joseph.edwardsrean@outlook.com>
2022-10-06 09:35:15 +0100

    Made template pages and created build folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
-import Painting from './pages/painting';
 import Navbar from './components/navbar';
+import Painting from './pages/painting';
+import Gaming from './pages/gaming';
+
 
 function App() {
   return (
@@ -14,6 +16,18 @@ function App() {
             <Route 
               path='/'
               element={<Home/>}
+              />
+           <Route 
+              path='/software'
+              element={<Home/>}
+              />
+            <Route 
+              path='/painting'
+              element={<Painting/>}
+              />
+            <Route
+              path='/gaming'
+              element={<Gaming/>}
               />
           </Routes>
         </div>
---
+++ frontend/src/pages/gaming.js
@@ -0,0 +1,8 @@
+const Gaming = () => {
+    return (
+        <div className='gaming'>
+            <h2>Gaming</h2>
+        </div>
+    )
+}
+export default Gaming;
