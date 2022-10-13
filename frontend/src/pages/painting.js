ea7bb123bb4bb7bbe5d3c84ed988da0a85477a92
Joseph Edwards <joseph.edwardsrean@outlook.com>
2022-10-13 15:31:21 +0100

    Making some style designs in index.css

--- frontend/src/pages/painting.js
+++ frontend/src/pages/painting.js
@@ -5,16 +5,24 @@ import {Route, BrowserRouter, Link} from 'react-router-dom';
 const Painting = () => {
     return (
         <div className='paintingNavigation'>
+            <div className='list-style'>
             <h2>Painting- Factions</h2>
+            </div>
+            <div className='list-style'>
             <Link to="/painting/DeathGuard">
                 <h1>Death Guard</h1>
             </Link>
+            </div>
+            <div className='list-style'>
             <Link to ="/painting/Orks">
                 <h1>Orks</h1>
             </Link>
+            </div>
+            <div className='list-style'>
             <Link to="/painting/AdeptaSororitas">
                 <h1>Adepta Sororitas</h1>
             </Link>
+            </div>
         </div>
     )
 }
